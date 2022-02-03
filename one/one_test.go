4a053a2ab3a768c2854a447e0f9abef488626110
wafer-bw <ben.jz.wakeford@gmail.com>
2022-02-03 17:17:23 -0500

    Fixes import path

--- one/one_test.go
+++ one/one_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"github.com/wafer-bw/gocovtesting/three"
+	"github.com/wafer-bw/go-test-cov-testing/three"
 )
 
 func TestOne(t *testing.T) {
